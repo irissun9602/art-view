1af7f058c214dc9fe5732c6fcb830f77f6c43b3d
dayoni <callmey@nate.com>
2018-05-16 20:29:16 +0900

    dto, mapper, controller 분류 src/main/resources/static , templates 폴더 생성 및 분류

---
+++ src/main/java/skhu/artview/controller/CommuController.java
@@ -0,0 +1,19 @@
+package skhu.artview.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api")
+public class CommuController {
+
+}
