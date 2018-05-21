3b3ae3612633b0c9e35df7ebcb9a4c4e626a2923
dayoni <callmey@nate.com>
2018-05-21 19:05:42 +0900

    api 링크 추가

--- src/main/java/skhu/artview/controller/MuseumController.java
+++ src/main/java/skhu/artview/controller/MuseumController.java
@@ -4,6 +4,7 @@ package skhu.artview.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,18 @@ import skhu.artview.model.Item;
 import skhu.artview.service.MuseumService;
 
 @RestController
+@CrossOrigin
+@RequestMapping("api")
 public class MuseumController {
-	
+
 	@Autowired
 	MuseumService service;
-	
+
 	@RequestMapping("bookList")
 		   public List<Item> bookList(@RequestParam(required=false)String keyword){
-	
+
 			keyword="java";
-	
+
 		       return service.searchItem(keyword,1,10);
 		   }
 }
