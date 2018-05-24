178fa4845b0221ffc6a0d99788b400e313d87f50
Ghayoung <wizardlover_@naver.com>
2018-05-24 16:25:57 +0900

    board dto

---
+++ src/main/java/skhu/artview/dto/Board.java
@@ -0,0 +1,35 @@
+package skhu.artview.dto;
+
+public class Board {
+
+	int id;
+	int projectId;
+	int userId;
+	int type;
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(int projectId) {
+		this.projectId = projectId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+
+}
