5de998c40eac289a1df5c1fdadf4c1d727836c4d
Ghayoung <wizardlover_@naver.com>
2018-06-02 19:04:37 +0900

    NettyProperties 추가

---
+++ src/main/java/skhu/artview/config/NettyProperties.java
@@ -0,0 +1,30 @@
+/*package skhu.artview.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "codebrain.server")
+public class NettyProperties {
+
+	private String host;
+
+	private Integer port = 7011;
+
+	public String getHost() {
+		return this.host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public Integer getPort() {
+		return this.port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+}
+*/
