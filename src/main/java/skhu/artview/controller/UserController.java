389b0f3082ac224286a0999b0b1e8873d8e41ba0
irissun9602 <iris9602@naver.com>
2018-06-04 23:26:28 +0900

    회원가입 컨트롤러 수정

--- src/main/java/skhu/artview/controller/UserController.java
+++ src/main/java/skhu/artview/controller/UserController.java
@@ -3,7 +3,10 @@ package skhu.artview.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,33 +18,46 @@ import skhu.artview.mapper.UserMapper;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+	
+	
+	@Autowired
+	 private UserMapper userMapper;
+	 private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Autowired UserMapper UserMapper;
+	    public UserController(UserMapper userMapper,
+	                          BCryptPasswordEncoder bCryptPasswordEncoder) {
+	        this.userMapper = userMapper;
+	        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	    }
 
-    @RequestMapping("list")
-    public List<User> list(Model model) {
-        List<User> users = UserMapper.findAll();
-
-        return users;
-    }
-
-    @RequestMapping(value="create", method=RequestMethod.GET)
+	    @PostMapping("/sign-up")
+	    public void signUp(@RequestBody User user) {
+	     user.setPwd(bCryptPasswordEncoder.encode(user.getPwd()));
+	        userMapper.insert(user);
+	    }
+	    
+	    @RequestMapping("/list")
+	    public List<User> list () {
+	    	return userMapper.findAll();
+	    }
+    /* 전민선: jwt 토큰 생성할 때 테스트했던 회원가입 컨트롤러(/sign-up)로 바꿨습니다.
+     * @RequestMapping(value="create", method=RequestMethod.GET)
     public User create(Model model) {
         User user = new User();
 
 
         return user;
-    }
+    
 
     @RequestMapping(value="create", method=RequestMethod.POST)
     public String create(Model model, User user) {
         UserMapper.insert(user);
         return "redirect:list";
-    }
+    }*/
 
     @RequestMapping(value="edit", method=RequestMethod.GET)
     public String edit(Model model, @RequestParam("id") int id) {
-        User user = UserMapper.findOne(id);
+        User user = userMapper.findOne(id);
 
         model.addAttribute("user", user);
 
@@ -50,13 +66,13 @@ public class UserController {
 
     @RequestMapping(value="edit", method=RequestMethod.POST)
     public String edit(Model model, User user) {
-        //UserMapper.update(User);
+        //userMapper.update(User);
         return "redirect:list";
     }
 
     @RequestMapping("delete")
     public String delete(Model model, @RequestParam("id") int id) {
-        UserMapper.delete(id);
+        userMapper.delete(id);
         return "redirect:list";
     }
 }
